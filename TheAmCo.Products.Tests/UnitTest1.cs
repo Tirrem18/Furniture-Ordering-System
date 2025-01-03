b6365261c82018d12e6c962369badb48645fa3d7
Tirrem <jacob.merritt18@gmail.com>
2025-01-03 21:32:26 +0000

    Tests

--- TheAmCo.Products.Tests/UnitTest1.cs
+++ TheAmCo.Products.Tests/UnitTest1.cs
@@ -1,11 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
-using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using ThAmCo.Products.Controllers;
 using ThAmCo.Products.Services.ProductsRepo;
+using TheAmCo.Products.Data.Products;
 
 namespace TheAmCo.Products.Tests
 {
@@ -23,25 +23,14 @@ namespace TheAmCo.Products.Tests
         }
 
         [TestMethod]
-        public void GetRestrictedData_ShouldReturnOkResult()
-        {
-            // Act
-            var result = _controller.GetRestrictedData();
-
-            // Assert
-            Assert.IsNotNull(result);
-            var okResult = result as OkObjectResult;
-            Assert.IsNotNull(okResult);
-            Assert.AreEqual(200, okResult.StatusCode);
-            Assert.AreEqual("Users can Order Products after implementation", 
-                            ((dynamic)okResult.Value).Message);
-        }
-
-        [TestMethod]
-        public async Task GetProducts_ShouldReturnOkResultWithProducts()
+        public async Task GetProducts_ShouldReturnOkWithProducts()
         {
             // Arrange
-            var fakeProducts = new List<string> { "Product1", "Product2" };
+            var fakeProducts = new List<Product>
+            {
+                new Product { Id = 1, Name = "Product1", Price = 100 },
+                new Product { Id = 2, Name = "Product2", Price = 200 }
+            };
             _mockProductsRepo.Setup(repo => repo.GetProductsAsync())
                              .ReturnsAsync(fakeProducts);
 
@@ -53,7 +42,25 @@ namespace TheAmCo.Products.Tests
             var okResult = result as OkObjectResult;
             Assert.IsNotNull(okResult);
             Assert.AreEqual(200, okResult.StatusCode);
-            CollectionAssert.AreEqual(fakeProducts, okResult.Value as List<string>);
+            Assert.AreEqual(fakeProducts, okResult.Value);
+        }
+
+        [TestMethod]
+        public async Task GetProducts_ShouldHandleNoProductsGracefully()
+        {
+            // Arrange
+            _mockProductsRepo.Setup(repo => repo.GetProductsAsync())
+                             .ReturnsAsync(new List<Product>());
+
+            // Act
+            var result = await _controller.GetProducts();
+
+            // Assert
+            Assert.IsNotNull(result);
+            var okResult = result as OkObjectResult;
+            Assert.IsNotNull(okResult);
+            Assert.AreEqual(200, okResult.StatusCode);
+            Assert.AreEqual(0, ((List<Product>)okResult.Value).Count);
         }
 
         [TestMethod]
@@ -89,7 +96,7 @@ namespace TheAmCo.Products.Tests
             var statusCodeResult = result as ObjectResult;
             Assert.IsNotNull(statusCodeResult);
             Assert.AreEqual(500, statusCodeResult.StatusCode);
-            Assert.AreEqual("An unexpected error occurred: Unexpected error", 
+            Assert.AreEqual("An unexpected error occurred: Unexpected error",
                             statusCodeResult.Value);
         }
     }
